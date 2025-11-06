22395c83989ed2dc41af1beee11660e2233c396d
abhishekranjan-cyber <abhishek.ranjan@easycred.co.in>
2025-11-06 13:51:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express'); 
+var app = express();
+const port = 3000;
+
+
+app.use(function(req,resp,next){
+
+    console.log('Logging');
+    next();
+
+});
+
+app.set("PORT", 3000); 
+app.set('view engine', 'ejs');
+app.use("/",express.static("public"));
+
+
+app.get("/home", function(req, res) {
+    const userName = "EasyCred"
+    const message = "Welcome to EasyCred, where loans are made easy";
+   
+    res.render('home', { name: userName, msg: message});
+});
+
+app.get("/api/user", function(req, res){
+    res.json({ name: "EasyCred", message: "Welcome to EasyCred from Client Side!"});
+});
+
+app.post("/post/form",function(req,resp){
+    console.log("/post/form");
+    resp.sendStatus(200); 
+});
+
+app.listen(app.get("PORT"),function(){
+    console.log('Server Running on Port ' + app.get("PORT"));
+});
+
+
+
+
+
+
